38b8365ea79b111d7976baeb57ed21b2795b2348
NOURZAIED <zaiednour7@gmail.com>
2023-01-04 00:25:16 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
+const data =[{
+    id : uuidv4() ,
+    title : "Creed",
+    description : "Released in 2015, Adonis Johnson, the son of heavyweight champion Apollo Creed, embraces his legacy as a boxer and seeks mentorship from Rocky Balboa, his father's old friend and rival.",
+    posterUrl : "https://flxt.tmsimg.com/assets/p11491087_p_v8_ad.jpg",
+    trailerUrl : "https://www.youtube.com/watch?v=Uv554B7YHk4",
+    rating : 4
+  },{
+    id : uuidv4(),
+    title : "Wrath of man",
+    description : "Released in 2021, Mysterious and wild-eyed, a new security guard for a cash truck surprises his co-workers when he unleashes precision skills during a heist. The crew is left wondering who he is and where he came from. Soon, the marksman's ultimate motive becomes clear as he takes dramatic and irrevocable steps to settle a score.",
+    posterUrl : "https://wwwimage-us.pplusstatic.com/thumbnails/photos/w370-q80/movie_asset/87/18/37/wom_salone_poster_1400x2100.jpg",
+    trailerUrl: "https://www.youtube.com/watch?v=EFYEni2gsK0",
+    rating : 5 ,
+    
+  },
+  {
+    id : uuidv4(),
+    title : "Troll",
+    description : "Released in 2022, When an ancient troll is awakened in a Norwegian mountain, a ragtag group of heroes must come together to try and stop it from wreaking deadly havoc.",
+    posterUrl : "https://m.media-amazon.com/images/M/MV5BYWUyNGEyZmQtZTc1NS00NDY0LTg1MzktY2JiYTkxYzNjNTBlXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    trailerUrl:'https://www.youtube.com/watch?v=AiohkY_XQYQ',
+    rating : 4
+  },
+  {
+    id : uuidv4(),
+    title : "Jhon Wick",
+    description : "Released in 2014, John Wick, a retired hitman, is forced to return to his old ways after a group of Russian gangsters steal his car and kill a puppy gifted to him by his late wife.",
+    posterUrl : "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_.jpg",
+    trailerUrl:'https://www.youtube.com/watch?v=pWK5crMuhHY',
+    rating : 4 
+  },
+{
+  id : uuidv4(),
+  title : "FBI",
+  description : " Released in 2006, A dedicated FBI agent recalls the agency's battles against the Klan, organized crime and Communist spies.",
+  posterUrl : "https://m.media-amazon.com/images/M/MV5BMTg5Mjg5NDk4OV5BMl5BanBnXkFtZTgwMjY1ODIyNjM@._V1_.jpg",
+  trailerUrl:'https://www.youtube.com/watch?v=JHnrlhyYViE',
+  rating : 7 },
+
+{
+  id : uuidv4(),
+  title : "the equalizer",
+  description : "Released in 2014.With his violent past behind him, McCall decides to lead a quiet life. However, when he sees a young girl, Teri, being controlled by violent gangsters, he once again takes up the fight for justice.",
+  posterUrl : "https://m.media-amazon.com/images/M/MV5BMTQ2MzE2NTk0NF5BMl5BanBnXkFtZTgwOTM3NTk1MjE@._V1_FMjpg_UX1000_.jpg",
+  trailerUrl:'https://www.youtube.com/watch?v=VjctHUEmutw',
+  rating : 3
+}
+];
+
+export default data
